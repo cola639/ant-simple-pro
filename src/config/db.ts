457ee67028395df7a33156971da151d629406df4
cola639 <337507950@qq.com>
2022-11-23 02:18:17 +0800

    FIRST COMMIT

---
+++ src/config/db.ts
@@ -0,0 +1,36 @@
+import mysql from 'mysql'
+import { code, sucessCallbackVal } from '../utils/variable'
+import { dbType } from '../interface'
+class mysqlDb<T> {
+  //创建数据库class
+  public connect() {
+    let connection = mysql.createPool({
+      //连接mysql配置
+      // host     : 'localhost',
+      host: 'localhost:3306',
+      user: 'root',
+      password: '123456',
+      database: 'admin_db',
+    })
+    return connection
+  }
+  public execute(sql: string, params?: any[] | object): Promise<dbType> {
+    //处理查询的逻辑
+    return new Promise((resolve, reject) => {
+      this.connect().getConnection((err, connection) => {
+        if (err) {
+          reject(err)
+        } else {
+          connection.query(sql, params, (err, rows) => {
+            err
+              ? reject(err)
+              : resolve(sucessCallbackVal(code.successCode, rows))
+            //connection.release();    // 结束会话
+            connection.destroy()
+          })
+        }
+      })
+    })
+  }
+}
+export default new mysqlDb()
